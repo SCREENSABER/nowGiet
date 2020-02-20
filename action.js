558e6867793e05b98c9683242ccd3d8baee710f9
SCREENSABER <61020218+SCREENSABER@users.noreply.github.com>
2020-02-20 09:08:43 -0600

    Fresh game.

---
+++ action.js
@@ -0,0 +1,8 @@
+// function greet(name='', age=30, pet='cat') {
+//  return `Hello ${name} you seem to be ${age}, What a lovely ${pet}`;
+// }
+// const array = [1, 2, 10, 16];
+// const mapArray = array.map((num) => {
+// 	return num * 2
+//});
+// console.log(mapArray);
